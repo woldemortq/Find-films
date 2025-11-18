576e4d5ba52424367bc3da05ab34bbd1b5384f8f
woldemortq <valeriy_valeriy322@mail.ru>
2025-11-18 13:50:19 +0500

    Тест

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 from api.routers.films_router import router as films_router
 from api.routers.auth_router import router as auth_router
 
-app = FastAPI(title="Serials and Films API")
+app = FastAPI(title="Поиск фильмов и сериалов")
 
 app.include_router(films_router)
 app.include_router(auth_router)
