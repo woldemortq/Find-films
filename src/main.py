da9c1f15136372328134d5e31ec8b61f41c493ff
woldemortq <valeriy_valeriy322@mail.ru>
2025-11-26 22:26:28 +0500

    перемещены некоторые файлы, поправил архитектуру

--- src/main.py
+++ src/main.py
@@ -15,4 +15,4 @@ app.include_router(favorites_router)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
